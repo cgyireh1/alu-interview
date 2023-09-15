037c5dfba07326291e8a00ad00199104315f15b1
cgyireh <c.gyireh@alustudent.com>
2023-09-15 16:50:03 -0700

    minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+"""Method calculates the least number of operations to result in exactly n H characters"""
+    min-operation = 0
+    factor = 2
+
+    while n > 1:
+        if n % factor == 0:
+            min-peration += factor
+            n //= factor
+        else:
+            factor += 1
+
+    return min-operation
